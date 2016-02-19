a85a5f8623d218721697b566ad07f761246796db
Jordan <jordanthomp81@gmail.com>
2016-02-18 23:15:00 -0600

    more ruler functions and styling;

--- app/app.module.js
+++ app/app.module.js
@@ -1,26 +1,65 @@
-new Vue({
+// new Vue({
+//
+//   // We want to target the div with an id of 'app'
+//   el: '#app',
+//
+//   // Here we can register any values or collections that hold data
+//   // for the application
+//   data: {
+//
+//   },
+//
+//   // Anything within the ready function will run when the application loads
+//   ready: function() {},
+//
+//   // Methods we want to use in our application are registered here
+//   methods: {
+//     // countTopRulerMarks : function (element) {
+//     //   var elementName = element.charAt(0) == '#' || '.' ? document.getElementById('foo') : document.getElementsByClassName('foo')[0];
+//     //   var elementInfo = element.getBoundingClientRect();
+//     //   var elementheight = elementInfo.height;
+//     //   var elementwidth = elementInfo.width;
+//     //   return elementwidth;
+//     // }
+//
+//     greet: function() {
+//       // `this` inside methods point to the Vue instance
+//       console.log('it worked');
+//     }
+//   }
+// });
 
-  // We want to target the div with an id of 'app'
+
+
+var vm = new Vue({
   el: '#app',
 
   // Here we can register any values or collections that hold data
   // for the application
   data: {
-
+    name: 'as'
   },
 
   // Anything within the ready function will run when the application loads
   ready: function() {},
 
-  // Methods we want to use in our application are registered here
+  // define methods under the `methods` object
   methods: {
+
     countTopRulerMarks : function (element) {
-      var elementName = element.charAt(0) == '#' || '.' ? document.getElementById('foo') : document.getElementsByClassName('foo')[0];
-      var elementInfo = element.getBoundingClientRect();
-      var elementheight = elementInfo.height;
-      var elementwidth = elementInfo.width;
-      return elementwidth;
+      var element = document.getElementById(element);
+      // var elementName = element.charAt(0) == '#' || '.' ? document.getElementById('foo') : document.getElementsByClassName('foo')[0];
+      // var elementInfo = element.getBoundingClientRect();
+      // var elementheight = elementInfo.height;
+      // var elementwidth = elementInfo.width;
+      console.log(element.offsetWidth);
+    },
+    greet: function() {
+      // `this` inside methods point to the Vue instance
+      this.name = 'jordan';
     }
   }
+})
 
-});
+// you can invoke methods in JavaScript too
+vm.countTopRulerMarks('top-ruler') // -> 'Hello Vue.js!'
--- src/js/ruler.js
+++ src/js/ruler.js
@@ -12,7 +12,62 @@
 //
 // }(jQuery));
 
-// $(document).ready(function () {
-//   //your code here
-//   console.log('ready now')
-// });
+$(document).ready(function () {
+
+  function buildTopRuler() {
+    $('.top-ruler').append('<div class="top-pointer"></div>').css('left', '0');
+    $('.left-ruler').append('<div class="left-pointer"></div>').css('top', '0');
+    var width = $('.top-ruler').width();
+    var numOfMarks = Math.round(width / 100);
+    var decimal = parseInt(String(width / 100).split('.')[1]) + 1;
+    var currSet = 0;
+
+    for (var i = 0; i < numOfMarks; i++) {
+
+      var sizeFlag = true;
+      for (var x = 0; x <= 18; x++) {
+        if(sizeFlag == true) {
+          // create five
+          $('.top-ruler').append('<div class="ruler-five"></div>');
+          sizeFlag = false;
+        }else {
+          // create ten
+          $('.top-ruler').append('<div class="ruler-ten"></div>');
+          sizeFlag = true;
+        }
+      }
+
+      currSet += 100;
+      var next = i + 1;
+      if(next < numOfMarks) {
+        $('.top-ruler').append('<div class="ruler-hundred"><h6 class="number">' + currSet + '</h6></div>');
+      }else {
+        $('.top-ruler').append('<div class="ruler-hundred"></div>');
+      }
+    }
+
+    var sizeFlag = true;
+    for (var j = 0; j < decimal; j++) {
+      if(sizeFlag == true) {
+        // create five
+        $('.top-ruler').append('<div class="ruler-five"></div>');
+        sizeFlag = false;
+      }else {
+        // create ten
+        // $('.top-ruler').append('<div class="ruler-ten"></div>');
+        sizeFlag = true;
+      }
+    }
+  }
+
+  $(document).on("mousemove", function(event) {
+    var offset = $('#app').offset();
+    var relativeX = (event.pageX - offset.left);
+    var relativeY = (event.pageY - offset.top);
+    console.log('moved')
+    $('.top-pointer').css('left', relativeX - 33);
+    $('.left-pointer').css('top', relativeY - 33);
+  });
+
+  buildTopRuler()
+});
