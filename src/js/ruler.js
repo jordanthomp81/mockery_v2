d2995e9152e4ba10e8d625b254438569db4509f2
Jordan <jordanthomp81@gmail.com>
2016-03-13 23:09:43 -0500

    added more elements to quick panel; tweaked animations;

--- src/js/ruler.js
+++ src/js/ruler.js
@@ -65,21 +65,46 @@ $(document).ready(function ($) {
     if(!panelOpen) {
       panelOpen = true;
       $(this).css("transform", "rotate(-45deg)");
+      $('.quick-panel-elements').removeClass('resolved');
+      $('.quick-panel-layers').removeClass('resolved');
+      $('.quick-panel-undo').removeClass('resolved');
+      $('.quick-panel-redo').removeClass('resolved');
       $('.quick-panel').animate({
         height: "60px"
-      }, 250, function() {
+      }, 150, function() {
         // Animation complete.
       });
     }else {
       panelOpen = false;
       $(this).css("transform", "rotate(0deg)");
       // $(this).removeClass('resolve');
+      $('.quick-panel-elements').addClass('resolved');
+      $('.quick-panel-layers').addClass('resolved');
+      $('.quick-panel-undo').addClass('resolved');
+      $('.quick-panel-redo').addClass('resolved');
       $('.quick-panel').animate({
-        height: "300px"
-      }, 250, function() {
+        height: "320px"
+      }, 150, function() {
         // Animation complete.
       });
     }
+  });
+
+  $('.quick-panel-undo').click(function() {
+    $('.quick-panel-undo').addClass('active');
+    setTimeout(function(){
+      console.log('timed');
+      $('.quick-panel-undo').removeClass('active');
+    }, .0100);
+  });
+
+  $('.quick-panel-redo').click(function() {
+    console.log('Fired');
+    $('.quick-panel-redo').addClass('active');
+    setTimeout(function(){
+      console.log('timed');
+      $('.quick-panel-redo').removeClass('active');
+    }, .0100);
   });
 
   createRuler('left');
