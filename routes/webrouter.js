ba651a8ae89838f741050334a1fc889b44dc1012
jordanthomp81 <jordanthomp81@gmail.com>
2016-11-15 13:51:46 -0600

    cleaned up code;

--- routes/webrouter.js
+++ routes/webrouter.js
@@ -2,13 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 router.route('/')
-    .get(function(req, res) {
-        res.sendFile('/index.html', { root: './public' });
-    });
+  .get(function(req, res) {
+    res.sendFile('/index.html', { root: './public' });
+  });
 
 router.route('/mockery')
-    .get(function(req, res) {
-        res.sendFile('/mockery.html', { root: './public' });
-    });
+  .get(function(req, res) {
+    res.sendFile('/mockery.html', { root: './public' });
+  });
 
 module.exports = router;
