4e2b6d60668888251200432bb93b315d80507371
jordanthomp81 <jordanthomp81@gmail.com>
2017-02-06 17:04:43 -0600

    more rework of active element border;

--- static/app/src/js/custom/element-templates.js
+++ static/app/src/js/custom/element-templates.js
@@ -6,18 +6,30 @@
 //
 
 function circleTemplate() {
-  var element = $('<div class="element-container draggable resizable"><div class="circle element"></div><span></span><span></span><span></span><span></span><div class="animated fadeInUp sub-menu"></div></div>');
+  var element = $('<div class="element-container shape draggable resizable"><div class="circle element"></div><span></span><span></span><span></span><span></span><div class="animated fadeInUp sub-menu"></div></div>');
   return element;
 }
 
+function circleInit(currEl) {
+
+}
+
 function rectangleTemplate() {
-  var element = $('<div class="element-container draggable resizable"><div class="rectangle element"></div><span></span><span></span><span></span><span></span><div class="animated fadeInUp sub-menu"></div></div>');
+  var element = $(
+  '<div class="element-container shape draggable resizable">' +
+    '<div class="rectangle element"></div>' +
+    '<span class="border-ball-one"></span><span class="border-ball-two"></span><span class="border-ball-three"></span><span class="border-ball-four"></span>' +
+  '</div>');
   return element;
+}
+
+function rectangleInit(currEl) {
+
 }
 
 function textTemplate() {
   var element = $(
-  '<div class="element-container text-container draggable">' +
+  '<div class="element-container content text-container draggable">' +
     '<p class="text element">Double Click to edit text</p>' +
     '<span class="border-ball-one"></span><span class="border-ball-two"></span><span class="border-ball-three"></span><span class="border-ball-four"></span>' +
     '<div class="animated fadeInUp sub-menu">' +
@@ -82,14 +94,24 @@ function textInit(currEl) {
 }
 
 function imageTemplate() {
-  var element = $('<div class="element-container draggable resizable"><div class="image element"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60"><path fill="#ecf0f1" d="M1 4.5h55v42H1z"/><path d="M57 47.5H0v-44h57v44zm-55-2h53v-40H2v40z" fill="#545e73"/><path fill="#545e73" d="M5 8.5h47v34H5z"/><path d="M53 43.5H4v-36h49v36zm-47-2h45v-32H6v32z" fill="#ecf0f1"/><circle cx="15" cy="17.069" r="4.569" fill="#f3d55a"/><path fill="#11a085" d="M51 32.611L50 31.5l-12-11L27.5 32l5.483 5.483L37 41.5h14z"/><path fill="#26b999" d="M6 41.5h31l-4.017-4.017-10.966-10.966L6 40.5z"/><circle cx="48" cy="44.5" r="12" fill="#71c386"/><path d="M54 43.5h-5v-5a1 1 0 0 0-2 0v5h-5a1 1 0 0 0 0 2h5v5a1 1 0 0 0 2 0v-5h5a1 1 0 0 0 0-2z" fill="#fff"/></svg></div><span></span><span></span><span></span><span></span><div class="animated fadeInUp sub-menu"></div></div>');
+  var element = $(
+  '<div class="element-container shape image-container draggable resizable">' +
+    '<div class="image element">' +
+      '<svg class="" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 60 60"><path fill="#ecf0f1" d="M1 4.5h55v42H1z"/><path d="M57 47.5H0v-44h57v44zm-55-2h53v-40H2v40z" fill="#545e73"/><path fill="#545e73" d="M5 8.5h47v34H5z"/><path d="M53 43.5H4v-36h49v36zm-47-2h45v-32H6v32z" fill="#ecf0f1"/><circle cx="15" cy="17.069" r="4.569" fill="#f3d55a"/><path fill="#11a085" d="M51 32.611L50 31.5l-12-11L27.5 32l5.483 5.483L37 41.5h14z"/><path fill="#26b999" d="M6 41.5h31l-4.017-4.017-10.966-10.966L6 40.5z"/><circle cx="48" cy="44.5" r="12" fill="#71c386"/><path d="M54 43.5h-5v-5a1 1 0 0 0-2 0v5h-5a1 1 0 0 0 0 2h5v5a1 1 0 0 0 2 0v-5h5a1 1 0 0 0 0-2z" fill="#fff"/></svg>' +
+    '</div>' +
+    '<span class="border-ball-one"></span><span class="border-ball-two"></span><span class="border-ball-three"></span><span class="border-ball-four"></span>' +
+  '</div>');
   return element;
+}
+
+function imageInit() {
+
 }
 
 function linkTemplate() {
   // var element = $('<div class="element-container draggable"><h6 class="link element draggable">This is a link</h6><span></span><span></span><span></span><span></span><div class="animated fadeInUp sub-menu"></div></div>');
   var element = $(
-  '<div class="element-container link-container draggable">' +
+  '<div class="element-container content link-container draggable">' +
     '<p class="link element">This is a link</p>' +
     '<span class="border-ball-one"></span><span class="border-ball-two"></span><span class="border-ball-three"></span><span class="border-ball-four"></span>' +
     '<div class="animated fadeInUp sub-menu">' +
@@ -148,7 +170,7 @@ function linkInit() {
 
 function checkboxTemplate() {
   var element = $(
-  '<div class="element-container checkbox-container draggable">' +
+  '<div class="element-container content checkbox-container draggable">' +
     '<label class="checkbox element"><input class="checkbox" type="checkbox" />This is a checkbox</label>' +
     '<span class="border-ball-one"></span><span class="border-ball-two"></span><span class="border-ball-three"></span><span class="border-ball-four"></span>' +
     '<div class="animated fadeInUp sub-menu">' +
@@ -212,7 +234,7 @@ function checkboxInit() {
 }
 
 function tableTemplate() {
-  var element = $('<div class="element-container draggable resizable"><table class="table element"><tr><td>Jill</td><td>Smith</td><td>50</td></tr><tr><td>Eve</td><td>Jackson</td><td>94</td></tr><tr><td>John</td><td>Doe</td><td>80</td></tr></table><span></span><span></span><span></span><span></span><div class="animated fadeInUp sub-menu"></div></div>');
+  var element = $('<div class="element-container shape draggable resizable"><table class="table element"><tr><td>Jill</td><td>Smith</td><td>50</td></tr><tr><td>Eve</td><td>Jackson</td><td>94</td></tr><tr><td>John</td><td>Doe</td><td>80</td></tr></table><span></span><span></span><span></span><span></span><div class="animated fadeInUp sub-menu"></div></div>');
   return element;
 }
 
@@ -224,7 +246,7 @@ function horizontalListInit() {
 
 function horizontalListTemplate() {
   var element = $(
-  '<div class="element-container checkbox-container draggable">' +
+  '<div class="element-container content checkbox-container draggable">' +
     '<h6 class="horizontal-list element">One | Two | Three</h6>' +
     '<span class="border-ball-one"></span><span class="border-ball-two"></span><span class="border-ball-three"></span><span class="border-ball-four"></span>' +
     '<div class="animated fadeInUp sub-menu">' +
@@ -282,7 +304,7 @@ function horizontalListTemplate() {
 
 function buttonTemplate() {
   var element = $(
-  '<div class="element-container button-container draggable">' +
+  '<div class="element-container content button-container draggable">' +
     '<div class="button element"><h6>Click Me!</h6></div>' +
     '<span class="border-ball-one"></span><span class="border-ball-two"></span><span class="border-ball-three"></span><span class="border-ball-four"></span>' +
     '<div class="animated fadeInUp sub-menu">' +
